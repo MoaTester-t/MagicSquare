9b79b3ab56795559febcaf93eb1bf1e213e7dede
Timo <40557627+TTimoTester@users.noreply.github.com>
2019-08-14 14:05:43 +0900

    Update OddMagicSquare.java

--- MagicSquqre/src/kr/com/moa/magicsquare/OddMagicSquare.java
+++ MagicSquqre/src/kr/com/moa/magicsquare/OddMagicSquare.java
@@ -2,81 +2,56 @@ package kr.com.moa.magicsquare;
 
 public class OddMagicSquare {
 
-	protected int n;
-
-	protected int magicArray[][];
-
-	/**
-	 * 마방진 배열 생성입력 받은 n사이즈 만큼의 배열을 만든다
-	 */
-	protected void makeMagicSquare() {
-		magicArray = new int[this.n][this.n];
-		int k = 1;
-		for (int i = 0; i < magicArray.length; i++) {
-			for (int j = 0; j < magicArray[i].length; j++) {
-//				magicArray[i][j] = k++;
-				magicArray[i][j] =1;
-			}
-		}
-		magicArray[n-1][n-2]=12;
+	int magicSquareSize = 0;
+	int magicSquare[][] = new int[this.magicSquareSize][this.magicSquareSize];
+	
+	void setSize(int _n) {
+		this.magicSquareSize = _n;
 	}
-
-	/**
-	 * 마방진 출력
-	 */
-	public void printSquare() {
-		System.out.println("~~~magic~~~[" + n + "]열  마방진");
-
-		try {
-			if (magicArray == null) {
-				System.out.println("size is zero");
-				return;
+	
+	void makeMagicSquare() {
+		
+		int arrI = magicSquareSize - 1;
+		int arrJ = magicSquareSize / 2;
+		int count = 1;
+		
+		magicSquare[arrI][arrJ] = count++;
+		
+		while(true) {
+			if (magicSquare[arrI+1][arrJ+1] == 0) {
+				magicSquare[++arrI][++arrJ] = count++;
 			}
-
-			for (int i = 0; i < magicArray.length; i++) {
-				for (int j = 0; j < magicArray[i].length; j++) {
-					System.out.print(magicArray[i][j] + "\t");
+			else {
+				arrI--;
+				if (arrI == -1) {
+					arrI = magicSquareSize - 1;
 				}
-				System.out.println();
+				if(magicSquare[arrI][arrJ] != 0) {
+					break;
+				}
+				magicSquare[arrI][arrJ] = count++;
 			}
-		} finally {
-			System.out.println("~~~magic~~~");
+		}
+		initArr();
+		
+	}
+	
+	void initArr() {
+		
+		for (int i= 0; i < this.magicSquareSize; i++) {
+			for(int j = 0; j < this.magicSquareSize; j++) {
+				this.magicSquare[i][j] = 0;
+			}
+		}	
+	}
+	
+	void printArr() {
+		for (int i= 0; i < this.magicSquareSize; i++) {
+			for(int j = 0; j < this.magicSquareSize; j++) {
+				System.out.println(magicSquare[i][j] + " ");
+			}
+			System.out.println();
 		}
 	}
-
-	public void setSize(int i) {
-		this.n = i;
-	}
-
-	public boolean isValidate() {
-
-		int index = 0;
-		int temp = 0;
-		int arr[] = new int[this.n + this.n];
-		for (int i = 0; i < this.n; i++) {
-			temp = 0;
-			for (int j = 0; j < this.n; j++) {
-
-				temp += magicArray[i][j];
-			}
-			arr[index++] = temp;
-		}
-
-		for (int j = 0; j < this.n; j++) {
-			temp = 0;
-			for (int i = 0; i < this.n; i++) {
-
-				temp += magicArray[j][i];
-			}
-			arr[index++] = temp;
-		}
-
-		for (int i = 1; i < this.n*2; i++) {
-			if (arr[i - 1] != arr[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
-
+	
 }
